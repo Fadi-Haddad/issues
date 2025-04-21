cbe7ac9b47848b276d59b9dbd6f1a2c5545848dd
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-04-21 17:22:39 +0300

    Made parse function understand count as an integer instead of string

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -24,7 +24,7 @@ defmodule CLI do
                                 aliases: [h: :help])
      case parse do
       {[help: :true], _, _} -> :help
-      {_, [user, project, count], _} -> [user, project, count]
+      {_, [user, project, count], _} -> [user, project, String.to_integer(count)]
       {_, [user, project], _} -> [user, project, @default_count]
       _ -> :help
      end
