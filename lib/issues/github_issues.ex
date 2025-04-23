1f9bb0bc385b4ac9c3f0dbebddd506dbde2ddb6b
Fadi Haddad <125252809+Fadi-Haddad-235@users.noreply.github.com>
2025-04-23 17:31:20 +0300

    Replaced HTTPosion with Tesla

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,24 +1,23 @@
 defmodule Issues.GithubIssues do
-
-  @user_agent [{"User-Agent", "ElixirClient"}]
-
   def create_link(user, project) do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
   def fetch(user, project) do
-    create_link(user, project)
-    |> HTTPoison.get(@user_agent)
-    |> handle_response
-  end
-  def handle_response({:error, %HTTPoison.Error{reason: reason}}) do
-    IO.inspect "error was encountered, reason is #{reason}"
-  end
-  def handle_response({:ok, %HTTPoison.Response{status_code: 404}}) do
-    IO.puts "URL not found :("
-  end
-  def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
-    IO.puts body
+    Tesla.client([{Tesla.Middleware.Headers, [{"User-Agent", "ElixirClient"}]}])
+    |> Tesla.get(create_link(user, project))
+    |> handle_response()
   end
 
+  def handle_response({:error, reason}) do
+    IO.inspect("Error was encountered, reason is: #{inspect(reason)}")
+  end
+
+  def handle_response({:ok, %Tesla.Env{status: 404}}) do
+    IO.puts("URL not found :(")
+  end
+
+  def handle_response({:ok, %Tesla.Env{status: 200, body: body}}) do
+    IO.inspect(body)
+  end
 end
